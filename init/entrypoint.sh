d40f3bc5bb0dcfac824e8f4d222f2825936f07af
George Gatheca <gatheca.george@gmail.com>
2024-12-02 11:34:41 +0000

    add init volume

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # Check if DB is already initialized
-if [ -f "/var/lib/merlin/db_initialized.txt" ]; then
+if [ -f "/merlin/db_initialized.txt" ]; then
   echo "Db initialized already"
   exit 0
 fi
@@ -21,13 +21,15 @@ else
     sed -i 's/TO_REPLACE_HERE/development/' ./snapshot_restore.toml
 fi
 
-wget -c -O state_db.sql.tar.gz  https://rpc-snapshot.merlinchain.io/${snapshot_prefix}state_db.sql.tar.gz --progress=dot
-wget -c -O prover_db.sql.tar.gz https://rpc-snapshot.merlinchain.io/${snapshot_prefix}prover_db.sql.tar.gz --progress=dot
+wget -c -O /merlin/state_db.sql.tar.gz  https://rpc-snapshot.merlinchain.io/${snapshot_prefix}state_db.sql.tar.gz --progress=dot
+wget -c -O /merlin/prover_db.sql.tar.gz https://rpc-snapshot.merlinchain.io/${snapshot_prefix}prover_db.sql.tar.gz --progress=dot
 
-./build restore --cfg ./snapshot_restore.toml -is ./state_db.sql.tar.gz -ih ./prover_db.sql.tar.gz
+./build restore --cfg ./snapshot_restore.toml -is /merlin/state_db.sql.tar.gz -ih /merlin/prover_db.sql.tar.gz
 
 # Execute script.sql for extra index for state db
 PGPASSWORD="state_password" psql -h cdk-validium-state-db -U state_user -d state_db -f /tmp/script.sql
 
 # Mark DB as initialized
-echo "done" > /var/lib/merlin/db_initialized.txt
+echo "done" > /merlin/db_initialized.txt
+rm /merlin/state_db.sql.tar.gz
+rm /merlin/prover_db.sql.tar.gz
